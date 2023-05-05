020d1bed337826cc28dd823e08057e866f93f8c3
anum-imm <132361505+anum-imm@users.noreply.github.com>
2023-05-05 17:57:16 +0500

    Add files via upload

---
+++ 2d.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a1[3][3];
+int a2[3][3]={{1,1,1},{1,1,1},{1,1,1}};
+int a3[3][3];
+for(int i=0;i<3;i++)
+{
+for(int j=0;j<3;j++)
+{
+    cout<<a2[i][j]<<" ";
+}
+cout<<endl;
+}
+for(int i=0;i<3;i++)
+{
+for(int j=0;j<3;j++)
+{ 
+    cin>>a1[i][j];
+        if (i==j)
+    {     
+    a1[i][j]+=a2[i][j];}
+}
+cout<<endl;
+}
+for(int i=0;i<3;i++)
+{
+for(int j=0;j<3;j++)
+{ 
+    cout<<a1[i][j];
+}
+cout<<endl;
+}
+}
