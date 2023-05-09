9ca1549f63f6bf607a5be4bbfd0c970b621b1d4b
Abeeranajam <132343991+Abeeranajam@users.noreply.github.com>
2023-05-09 10:45:21 +0500

    Update 2d.cpp

--- 2d.cpp
+++ 2d.cpp
@@ -3,18 +3,7 @@ using namespace std;
 int main()
 {
 int a1[3][3];
-int a2[3][3]={{1,1,1},{1,1,1},{1,1,1}};
-int a3[3][3];
-for(int i=0;i<3;i++)
-{
-for(int j=0;j<3;j++)
-{
-    cout<<a2[i][j]<<" ";
-}
-cout<<endl;
-}
-for(int i=0;i<3;i++)
-{
+int 
 for(int j=0;j<3;j++)
 { 
     cin>>a1[i][j];
@@ -30,9 +19,5 @@ for(int j=0;j<3;j++)
     cout<<a1[i][j];
    
 }
-    for ( int i=0; i<5; i++)
-        cout<<endl;
-    cout<<endl;
-    
-}
-}
+    for ( int i=0; i<5;
+     abeeeeeeeeeeeerrrrrrrrraaaaaaaaaaaa
