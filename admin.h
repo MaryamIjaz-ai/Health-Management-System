03b793de3a1ee4b090df33e49eb77d33ae102ca5
Anum Imran <anumimran168@gmail.com>
2023-05-14 19:10:57 +0500

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,26 +1,31 @@
 #include<iostream>
+<<<<<<< Updated upstream
+#include"user.h"
+#include"patient.h"
+=======
 #include<fstream>
-#include "admin.h";
-#include "Appointment.h";
-#include "Doctor.h";
-#include "patient.h";
-#include "User.h";
-#include "Hospital.h";
-
+#include"User.h"
+>>>>>>> Stashed changes
 using namespace std;
-class Admin:public User {
+
+class Admin:public user{
     protected:
     int staffID;
     public:
-    Admin(){}
-       Admin( string cnic,string name,int age,string phone_no,string email,string password,int staffID){}
+    Admin()
+	{
 
-    int setstaffid(int staffID)
-    { 
-      staffID=staffID;
-    }
-    int staffid()
-   {
-      return staffID;
-}
+	}
+	Admin(string cnic_n, string name, int age, string phone_no, string email, string password, int staffID)
+	{
+		staffID = staffID;
+	}
+	int setstaffid(int staffID)
+	{
+		staffID = staffID;
+	}
+	int getstaffid()
+	{
+		return staffID;
+	}
 };
