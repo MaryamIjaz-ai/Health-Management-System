89ece9bbda493c8d75752bd7c8641041c36f87c1
Anum Imran <anumimran168@gmail.com>
2023-05-14 19:10:53 +0500

    Update appointment.h

--- appointment.h
+++ appointment.h
@@ -1,12 +1,11 @@
+
 #pragma once
 #include<iostream>
-#include<fstream>
-#include "admin.h";
-#include "Appointment.h";
-#include "Doctor.h";
-#include "patient.h";
-#include "User.h";
-#include "Hospital.h";
+
+#include"Admin.h"
+#include"Doctor.h"
+#include"patient.h"
+#include"Payment.h"
 
 class Appointment
 {
@@ -89,39 +88,6 @@ public:
 			}
 		}
 	}
-else if (mod == "    In-Person_Consultation")
-		{
-			cout << mod << endl;
-			cout << "     Total Bill: 2500rs" << endl;
-			cout << "        1. Pay Online now." << endl;
-			cout << "        2. Pay Later." << endl;
-			cout << "        0. Cancel Appointmnet" << endl;
-			cout << "    Choose: ";
-			cin >> pay;
-			while (pay < 0 || pay>2)
-			{
-				cout << "    Invalid." << endl;
-				cout << "    Choose: ";
-				cin >> pay;
-			}
-			if (pay == 1)
-			{
-				cout << "    Online Payment Completed." << endl << endl;
-				return 1;
-			}
-			else if (pay == 2)
-			{
-				cout << "    Bill: 2500rs" << endl;
-				cout << "    Payment Pending." << endl << endl;
-				return 2;
-			}
-			else if (pay == 0)
-			{
-				return 0;
-			}
-		}
-	}
-
 	void Bookings(string docname, string patname, string patcnic)
 	{
 		string word, doctname, doctcnic, doctorUsername, doctorPassword, doctorno, doctoremail, doctorphoneno, city, hospital, experience, specification;
@@ -367,8 +333,10 @@ else if (mod == "    In-Person_Consultation")
 		{
 			cout << "    No Appointmnets" << endl << endl;
 		}
-	};
+	}
 
 
 
 
+
+};
