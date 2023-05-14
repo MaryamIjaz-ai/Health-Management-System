28c58b086a7494ff195e20503cd57227534126eb
Abeeranajam <132343991+Abeeranajam@users.noreply.github.com>
2023-05-14 17:42:41 +0500

    Update doctor.h

--- doctor.h
+++ doctor.h
@@ -5,7 +5,7 @@
 #include "Doctor.h";
 #include "patient.h";
 #include "User.h";
-#include "Hospital.h"
+#include "Hospital.h";
 using namespace std;
 class Doctor : public User         // inheritance from user class
 {
