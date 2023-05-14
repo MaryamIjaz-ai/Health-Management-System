4d82379c70b177b939a8e709a9c30bb7ecfb2941
Anum Imran <anumimran168@gmail.com>
2023-05-14 19:55:35 +0500

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,24 +1,30 @@
+#pragma once
 #include<iostream>
 #include<fstream>
-#include "Appointment.h";
-#include "Doctor.h";
-#include "patient.h";
-#include "User.h";
+#include"User.h"
+#include"Doctor.h"
+#include"patient.h"
+#include"Appointment.h"
 
-using namespace std;
-class Admin:public User {
-    protected:
-    int staffID;
-    public:
-    Admin(){}
-       Admin( string cnic,string name,int age,string phone_no,string email,string password,int staffID){}
 
-    int setstaffid(int staffID)
-    { 
-      staffID=staffID;
-    }
-    int staffid()
-   {
-      return staffID;
-}
+class Admin :public User{
+protected:
+	int staffID;
+public:
+	Admin()
+	{
+
+	}
+	Admin(string cnic_n, string name, int age, string phone_no, string email, string password, int staffID)
+	{
+		staffID = staffID;
+	}
+	int setstaffid(int staffID)
+	{
+		staffID = staffID;
+	}
+	int getstaffid()
+	{
+		return staffID;
+	}
 };
